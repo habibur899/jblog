55c59f9639bf98da82e4afc7f0b198e9416bb557
Habibur Rahman <habiburg3@gmail.com>
2021-08-29 12:18:52 +0600

    Theme Bootstraping

---
+++ functions.php
@@ -0,0 +1,80 @@
+<?php
+define('VERSION',wp_get_theme()->get( 'Version' ));
+if ( ! function_exists( 'jblog_theme_setup' ) ) :
+	function jblog_theme_setup() {
+		//load textdomain
+		load_theme_textdomain( 'jblog', get_template_directory() . '/languages' );
+		//feed
+		add_theme_support( 'automatic-feed-links' );
+		//title
+		add_theme_support( 'title-tag' );
+		//post thumbnails
+		add_theme_support( 'post-thumbnails' );
+		//register nav menus
+		register_nav_menus( array(
+			'primary_menu' => __( 'Primary Menu', 'jblog' ),
+			'footer_menu' => __( 'Footer Menu', 'jblog' ),
+		) );
+
+		//html5 support
+		add_theme_support(
+			'html5',
+			array(
+				'search-form',
+				'comment-form',
+				'comment-list',
+				'gallery',
+				'caption',
+				'style',
+				'script',
+			)
+		);
+		//post formate
+		add_theme_support( 'post-formats', array('aside','image','gallery','video','audio','link','quote','status') );
+		//custom background
+		add_theme_support(
+			'custom-background',
+			apply_filters(
+				'theme_name_custom_background_args',
+				array(
+					'default-color' => 'ffffff',
+					'default-image' => '',
+				)
+			)
+		);
+		//widgets refresh
+		add_theme_support( 'customize-selective-refresh-widgets' );
+		//logo
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 250,
+				'width'       => 250,
+				'flex-width'  => true,
+				'flex-height' => true,
+			)
+		);
+	}
+endif;
+add_action( 'after_setup_theme', 'jblog_theme_setup' );
+
+
+/* Enqueue scripts and styles.*/
+function jblog_assets() {
+	//css
+	wp_enqueue_style ( 'custom-google-font', '//fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&amp;display=swap');
+	wp_enqueue_style ( 'bootstrap-css', get_template_directory_uri().'/assets/css/bootstrap.min.css', array (),VERSION );
+	wp_enqueue_style ( 'LineIcons-css', get_template_directory_uri().'/assets/css/LineIcons.2.0.css', array (),VERSION );
+	wp_enqueue_style ( 'animate-css', get_template_directory_uri().'/assets/css/animate.css', array (),VERSION );
+	wp_enqueue_style ( 'tiny-slider-css', get_template_directory_uri().'/assets/css/tiny-slider.css', array (),VERSION );
+	wp_enqueue_style ( 'glightbox-css', get_template_directory_uri().'/assets/css/glightbox.min.css', array (),VERSION );
+	wp_enqueue_style ( 'main-css', get_template_directory_uri().'/assets/css/main.css', array (),VERSION );
+	//js
+	wp_enqueue_script( 'bootstrap-js',  get_template_directory_uri().'/assets/js/bootstrap.min.js', array(), VERSION, true );
+	wp_enqueue_script( 'wow-min-js',  get_template_directory_uri().'/assets/js/wow.min.js', array(), VERSION, true );
+	wp_enqueue_script( 'tiny-slider-js',  get_template_directory_uri().'/assets/js/tiny-slider.js', array(), VERSION, true );
+	wp_enqueue_script( 'glightbox-js',  get_template_directory_uri().'/assets/js/glightbox.min.js', array(), VERSION, true );
+	wp_enqueue_script( 'main-js',  get_template_directory_uri().'/assets/js/main.js', array(), VERSION, true );
+}
+add_action( 'wp_enqueue_scripts', 'jblog_assets' );
+
---
+++ index.php
@@ -0,0 +1,323 @@
+<?php get_header() ?>
+
+
+<section class="section latest-news-area blog-list">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-7 col-12">
+                <div class="row">
+                    <div class="col-lg-6 col-12">
+
+                        <div class="single-news wow fadeInUp" data-wow-delay=".3s">
+                            <div class="image">
+                                <img class="thumb"
+                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blog/blog1.jpg"
+                                     alt="#">
+                            </div>
+                            <div class="content-body">
+                                <h4 class="title"><a href="blog-single.html">The Internet Is A Job Seeker
+                                        Most Crucial Success</a></h4>
+                                <div class="meta-details">
+                                    <ul>
+                                        <li><a href="#"><i class="lni lni-tag"></i> Job skills</a></li>
+                                        <li><a href="#"><i class="lni lni-calendar"></i> 12-09-2023</a></li>
+                                        <li><a href="#"><i class="lni lni-eye"></i> 55</a></li>
+                                    </ul>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum
+                                    has been the standard.</p>
+                                <div class="button">
+                                    <a href="blog-single.html" class="btn">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6 col-12">
+
+                        <div class="single-news wow fadeInUp" data-wow-delay=".5s">
+                            <div class="image">
+                                <img class="thumb"
+                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blog/blog2.jpg"
+                                     alt="#">
+                            </div>
+                            <div class="content-body">
+                                <h4 class="title"><a href="blog-single.html">Today From Connecting With
+                                        Potential Employers</a></h4>
+                                <div class="meta-details">
+                                    <ul>
+                                        <li><a href="#"><i class="lni lni-tag"></i> Career advice</a></li>
+                                        <li><a href="#"><i class="lni lni-calendar"></i> 10-10-2023</a></li>
+                                        <li><a href="#"><i class="lni lni-eye"></i> 55</a></li>
+                                    </ul>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum
+                                    has been the standard.</p>
+                                <div class="button">
+                                    <a href="blog-single.html" class="btn">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6 col-12">
+
+                        <div class="single-news wow fadeInUp" data-wow-delay=".3s">
+                            <div class="image">
+                                <img class="thumb"
+                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blog/blog3.jpg"
+                                     alt="#">
+                            </div>
+                            <div class="content-body">
+                                <h4 class="title"><a href="blog-single.html">We’ve Weeded Through Hundreds
+                                        Of Job Hunting</a></h4>
+                                <div class="meta-details">
+                                    <ul>
+                                        <li><a href="#"><i class="lni lni-tag"></i> Future plan</a></li>
+                                        <li><a href="#"><i class="lni lni-calendar"></i> 09-05-2023</a></li>
+                                        <li><a href="#"><i class="lni lni-eye"></i> 55</a></li>
+                                    </ul>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum
+                                    has been the standard.</p>
+                                <div class="button">
+                                    <a href="blog-single.html" class="btn">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6 col-12">
+
+                        <div class="single-news wow fadeInUp" data-wow-delay=".5s">
+                            <div class="image">
+                                <img class="thumb"
+                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blog/blog1.jpg"
+                                     alt="#">
+                            </div>
+                            <div class="content-body">
+                                <h4 class="title"><a href="blog-single.html">The Internet Is A Job Seeker
+                                        Most Crucial Success</a></h4>
+                                <div class="meta-details">
+                                    <ul>
+                                        <li><a href="#"><i class="lni lni-tag"></i> Job skills</a></li>
+                                        <li><a href="#"><i class="lni lni-calendar"></i> 12-09-2023</a></li>
+                                        <li><a href="#"><i class="lni lni-eye"></i> 55</a></li>
+                                    </ul>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum
+                                    has been the standard.</p>
+                                <div class="button">
+                                    <a href="blog-single.html" class="btn">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6 col-12">
+
+                        <div class="single-news wow fadeInUp" data-wow-delay=".3s">
+                            <div class="image">
+                                <img class="thumb"
+                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blog/blog2.jpg"
+                                     alt="#">
+                            </div>
+                            <div class="content-body">
+                                <h4 class="title"><a href="blog-single.html">Today From Connecting With
+                                        Potential Employers</a></h4>
+                                <div class="meta-details">
+                                    <ul>
+                                        <li><a href="#"><i class="lni lni-tag"></i> Career advice</a></li>
+                                        <li><a href="#"><i class="lni lni-calendar"></i> 10-10-2023</a></li>
+                                        <li><a href="#"><i class="lni lni-eye"></i> 55</a></li>
+                                    </ul>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum
+                                    has been the standard.</p>
+                                <div class="button">
+                                    <a href="blog-single.html" class="btn">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6 col-12">
+
+                        <div class="single-news wow fadeInUp" data-wow-delay=".5s">
+                            <div class="image">
+                                <img class="thumb"
+                                     src="<?php echo get_template_directory_uri() ?>/assets/images/blog/blog3.jpg"
+                                     alt="#">
+                            </div>
+                            <div class="content-body">
+                                <h4 class="title"><a href="blog-single.html">We’ve Weeded Through Hundreds
+                                        Of Job Hunting</a></h4>
+                                <div class="meta-details">
+                                    <ul>
+                                        <li><a href="#"><i class="lni lni-tag"></i> Future plan</a></li>
+                                        <li><a href="#"><i class="lni lni-calendar"></i> 09-05-2023</a></li>
+                                        <li><a href="#"><i class="lni lni-eye"></i> 55</a></li>
+                                    </ul>
+                                </div>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum
+                                    has been the standard.</p>
+                                <div class="button">
+                                    <a href="blog-single.html" class="btn">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="pagination center">
+                    <ul class="pagination-list">
+                        <li><a href="#"><i class="lni lni-chevron-left"></i></a></li>
+                        <li class="active"><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#"><i class="lni lni-chevron-right"></i></a></li>
+                    </ul>
+                </div>
+
+            </div>
+            <aside class="col-lg-4 col-md-5 col-12">
+	            <?php get_sidebar()?>
+            </aside>
+        </div>
+    </div>
+</section>
+
+
+<div class="modal fade form-modal" id="login" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog max-width-px-840 position-relative">
+        <button type="button"
+                class="circle-32 btn-reset bg-white pos-abs-tr mt-md-n6 mr-lg-n6 focus-reset z-index-supper"
+                data-dismiss="modal"><i class="lni lni-close"></i></button>
+        <div class="login-modal-main">
+            <div class="row no-gutters">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="heading">
+                            <h3>Login From Here</h3>
+                            <p>Log in to continue your account <br> and explore new jobs.</p>
+                        </div>
+                        <div class="social-login">
+                            <ul>
+                                <li><a class="linkedin" href="#"><i class="lni lni-linkedin-original"></i> Log in
+                                        with LinkedIn</a></li>
+                                <li><a class="google" href="#"><i class="lni lni-google"></i> Log in with
+                                        Google</a></li>
+                                <li><a class="facebook" href="#"><i class="lni lni-facebook-original"></i> Log in
+                                        with Facebook</a></li>
+                            </ul>
+                        </div>
+                        <div class="or-devider">
+                            <span>Or</span>
+                        </div>
+                        <form action="https://demo.graygrids.com/">
+                            <div class="form-group">
+                                <label for="email" class="label">E-mail</label>
+                                <input type="email" class="form-control" placeholder="example@gmail.com" id="email">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="label">Password</label>
+                                <div class="position-relative">
+                                    <input type="password" class="form-control" id="password"
+                                           placeholder="Enter password">
+                                </div>
+                            </div>
+                            <div class="form-group d-flex flex-wrap justify-content-between">
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                                    <label class="form-check-label" for="flexCheckDefault">Remember password</label>
+                                </div>
+                                <a href="#" class="font-size-3 text-dodger line-height-reset">Forget Password</a>
+                            </div>
+                            <div class="form-group mb-8 button">
+                                <button class="btn ">Log in
+                                </button>
+                            </div>
+                            <p class="text-center create-new-account">Don’t have an account? <a href="#">Create a free
+                                    account</a></p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="modal fade form-modal" id="signup" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog max-width-px-840 position-relative">
+        <button type="button"
+                class="circle-32 btn-reset bg-white pos-abs-tr mt-md-n6 mr-lg-n6 focus-reset z-index-supper"
+                data-dismiss="modal"><i class="lni lni-close"></i></button>
+        <div class="login-modal-main">
+            <div class="row no-gutters">
+                <div class="col-12">
+                    <div class="row">
+                        <div class="heading">
+                            <h3>Create a free Account <br> Today</h3>
+                            <p>Create your account to continue <br> and explore new jobs.</p>
+                        </div>
+                        <div class="social-login">
+                            <ul>
+                                <li><a class="linkedin" href="#"><i class="lni lni-linkedin-original"></i> Import from
+                                        LinkedIn</a></li>
+                                <li><a class="google" href="#"><i class="lni lni-google"></i> Import from
+                                        Google</a></li>
+                                <li><a class="facebook" href="#"><i class="lni lni-facebook-original"></i> Import from
+                                        Facebook</a></li>
+                            </ul>
+                        </div>
+                        <div class="or-devider">
+                            <span>Or</span>
+                        </div>
+                        <form action="https://demo.graygrids.com/">
+                            <div class="form-group">
+                                <label for="email" class="label">E-mail</label>
+                                <input type="email" class="form-control" placeholder="example@gmail.com">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="label">Password</label>
+                                <div class="position-relative">
+                                    <input type="password" class="form-control" placeholder="Enter password">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="label">Confirm Password</label>
+                                <div class="position-relative">
+                                    <input type="password" class="form-control" placeholder="Enter password">
+                                </div>
+                            </div>
+                            <div class="form-group d-flex flex-wrap justify-content-between">
+
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="">
+                                    <label class="form-check-label" for="flexCheckDefault">Agree to the <a href="#">Terms
+                                            & Conditions</a></label>
+                                </div>
+                            </div>
+                            <div class="form-group mb-8 button">
+                                <button class="btn ">Sign Up
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer() ?>
